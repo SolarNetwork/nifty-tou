a84d592819ac471fe998404e5fa3ece5fc383535
Matt Magoffin <matt@solarnetwork.net>
2023-09-21 11:40:38 +1200

    Add ChronoFieldParser to help parse locale month and weekday names.

---
+++ src/main/ChronoFieldParser.ts
@@ -0,0 +1,198 @@
+/**
+ * An enumeration of supported chronological fields of the Gregorian calendar.
+ */
+export enum ChronoField {
+	/** The month of year, from January (1) to December (12). */
+	MONTH_OF_YEAR = 0,
+
+	/** The day of the week, from Monday (1) to Sunday (7). */
+	DAY_OF_WEEK = 1,
+}
+
+/**
+ * A chronological field value.
+ */
+export class ChronoFieldValue {
+	#field: ChronoField;
+	#names: string[];
+	#value;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param field - the chronological field
+	 * @param names - the value names, from longest to shortest
+	 * @param value - the value
+	 */
+	constructor(field: ChronoField, names: string[], value: number) {
+		this.#field = field;
+		this.#names = names;
+		this.#value = value;
+		if (!names || names.length < 1) {
+			throw new TypeError(`The names array must must not be empty.`);
+		}
+	}
+
+	/** Get the field. */
+	get field(): ChronoField {
+		return this.#field;
+	}
+
+	/** Get the full name. */
+	get name(): string {
+		return this.#names[0];
+	}
+
+	/** Get the short name. */
+	get shortName(): string {
+		return this.#names.length > 1 ? this.#names[1] : this.#names[0];
+	}
+
+	/** Get the value. */
+	get value(): number {
+		return this.#value;
+	}
+}
+
+/**
+ * Class to parse locale-specific chronological field names of the Gregorian calendar.
+ */
+export class ChronoFieldParser {
+	#locale: string;
+
+	// note the string keys in these maps are locale lower-case values to support
+	// case-insensitive matching
+	#values: Map<ChronoField, Map<string, ChronoFieldValue>>;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param locale - the desired locale
+	 */
+	constructor(locale: string) {
+		this.#locale = locale;
+		this.#values = new Map();
+
+		this.#computeMonths(locale);
+		this.#computeWeekdays(locale);
+	}
+
+	/**
+	 * Get the locale.
+	 */
+	get locale(): string {
+		return this.#locale;
+	}
+
+	/**
+	 * Compute a set of keys and names of a date field.
+	 *
+	 * @param date - the date to use for field formatting
+	 * @param names - the array to populate with field names, in the order of the given `formats`
+	 * @param formats - the list of formats to apply to date to generate field names
+	 * @returns array of date key names
+	 */
+	static #computeKeysAndNames(
+		date: Date,
+		names: string[],
+		...formats: Intl.DateTimeFormat[]
+	): string[] {
+		const keys: string[] = [];
+		for (const fmt of formats) {
+			// generate name and remove all punctuation, e.g. some short names include a period
+			const name = fmt.format(date).replace(/\p{P}/gu, "");
+			names.splice(names.length, 0, name);
+			keys.splice(keys.length, 0, name);
+
+			// generate a key version with diacritic characters replaced with non-accented version
+			// e.g. replace é with e
+			const norm = name.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+			if (norm !== name) {
+				keys.splice(keys.length, 0, norm);
+			}
+		}
+		return keys;
+	}
+
+	#computeMonths(locale: string) {
+		const intlFull = new Intl.DateTimeFormat(locale, {
+			month: "long",
+			timeZone: "UTC",
+		});
+		const intlShort = new Intl.DateTimeFormat(locale, {
+			month: "short",
+			timeZone: "UTC",
+		});
+		const values = new Map<string, ChronoFieldValue>();
+		for (let i = 0; i < 12; i += 1) {
+			const date = new Date(Date.UTC(2024, i, 1));
+			const val = i + 1;
+			const names = [];
+			const keys = ChronoFieldParser.#computeKeysAndNames(
+				date,
+				names,
+				intlFull,
+				intlShort
+			);
+			const value = new ChronoFieldValue(
+				ChronoField.MONTH_OF_YEAR,
+				names,
+				val
+			);
+			for (const key of keys) {
+				values.set(key.toLocaleLowerCase(locale), value);
+			}
+		}
+		this.#values.set(ChronoField.MONTH_OF_YEAR, values);
+	}
+
+	#computeWeekdays(locale: string) {
+		const intlFull = new Intl.DateTimeFormat(locale, {
+			weekday: "long",
+			timeZone: "UTC",
+		});
+		const intlShort = new Intl.DateTimeFormat(locale, {
+			weekday: "short",
+			timeZone: "UTC",
+		});
+		const values = new Map<string, ChronoFieldValue>();
+		for (let i = 1; i <= 7; i += 1) {
+			const date = new Date(Date.UTC(2024, 0, i)); // 2024-01-01 is a Monday
+			const names = [];
+			const keys = ChronoFieldParser.#computeKeysAndNames(
+				date,
+				names,
+				intlFull,
+				intlShort
+			);
+			const value = new ChronoFieldValue(
+				ChronoField.DAY_OF_WEEK,
+				names,
+				i
+			);
+			for (const key of keys) {
+				values.set(key.toLocaleLowerCase(locale), value);
+			}
+		}
+		this.#values.set(ChronoField.DAY_OF_WEEK, values);
+	}
+
+	/**
+	 * Parse a field value.
+	 *
+	 * @param field - the field to treat `val` as
+	 * @param value - the field value to parse
+	 * @returns the associated field value, or undefined if not found
+	 */
+	parse(field: ChronoField, value: string): ChronoFieldValue {
+		if (!value) {
+			return undefined;
+		}
+		const values = this.#values.get(field);
+		if (!values) {
+			throw new TypeError(`Unsupported ChronoField [${field}].`);
+		}
+		const lcValue = value.toLocaleLowerCase(this.#locale);
+		return values.get(lcValue);
+	}
+}
---
+++ src/test/ChronoFieldParserTests.ts
@@ -0,0 +1,492 @@
+import test from "ava";
+import {
+	ChronoField,
+	ChronoFieldValue,
+	ChronoFieldParser,
+} from "../main/ChronoFieldParser.js";
+
+test("ChronoFieldValue:construct", (t) => {
+	const f = ChronoField.MONTH_OF_YEAR;
+	const n = ["January", "Jan"];
+	const v = 1;
+	const o = new ChronoFieldValue(f, n, v);
+	t.is(o.field, f, "field from constructor arg");
+	t.is(o.name, n[0], "name from constructor arg");
+	t.is(o.shortName, n[1], "shortName from constructor arg");
+	t.is(o.value, v, "value from constructor arg");
+});
+
+test("ChronoFieldValue:construct:emptyNames", (t) => {
+	const f = ChronoField.MONTH_OF_YEAR;
+	const v = 1;
+	t.throws(
+		() => {
+			new ChronoFieldValue(f, undefined, v);
+		},
+		{ instanceOf: TypeError },
+		"undefined names array throws TypeError"
+	);
+	t.throws(
+		() => {
+			new ChronoFieldValue(f, [], v);
+		},
+		{ instanceOf: TypeError },
+		"empty names array throws TypeError"
+	);
+});
+
+test("ChronoFieldValue:noShortName", (t) => {
+	const f = ChronoField.MONTH_OF_YEAR;
+	const n = ["January"];
+	const v = 1;
+	const o = new ChronoFieldValue(f, n, v);
+	t.is(o.shortName, n[0], "full name returned when no short name avaialble");
+});
+
+test("ChronoFieldParser:construct:en-US", (t) => {
+	const locale = "en-US";
+	const p = new ChronoFieldParser(locale);
+	t.is(p.locale, locale, "locale from constructor arg");
+});
+
+test("ChronoFieldParser:parse:unknown", (t) => {
+	const p = new ChronoFieldParser("en-US");
+	t.throws(
+		() => {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			p.parse(-1, "Jan");
+		},
+		{ instanceOf: TypeError },
+		"invalid ChronoField value throws TypeError"
+	);
+});
+
+test("ChronoFieldParser:parse:month:undefined", (t) => {
+	const p = new ChronoFieldParser("en-US");
+	t.is(
+		p.parse(undefined, undefined),
+		undefined,
+		"undefined arguments parsed as undefined"
+	);
+	t.is(p.parse(null, null), undefined, "null arguments parsed as undefined");
+	t.is(
+		p.parse(ChronoField.MONTH_OF_YEAR, undefined),
+		undefined,
+		"undefined parsed as undefined"
+	);
+	t.is(
+		p.parse(ChronoField.MONTH_OF_YEAR, null),
+		undefined,
+		"null parsed as undefined"
+	);
+	t.is(
+		p.parse(ChronoField.MONTH_OF_YEAR, ""),
+		undefined,
+		"empty parsed as undefined"
+	);
+	t.is(
+		p.parse(ChronoField.MONTH_OF_YEAR, "Not A Month"),
+		undefined,
+		"unknown parsed as undefined"
+	);
+});
+
+test("ChronoFieldParser:parse:month:en-US", (t) => {
+	const locale = "en-US";
+	const p = new ChronoFieldParser(locale);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "January"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "January",
+			shortName: "Jan",
+			value: 1,
+		},
+		"full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "JaNuAry"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "January",
+			shortName: "Jan",
+			value: 1,
+		},
+		"full name parsed in case-insensitive manner"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "Jan"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "January",
+			shortName: "Jan",
+			value: 1,
+		},
+		"short name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "jAn"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "January",
+			shortName: "Jan",
+			value: 1,
+		},
+		"short name parsed in case insensitive manner"
+	);
+
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "February"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "February",
+			shortName: "Feb",
+			value: 2,
+		},
+		"February full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "March"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "March",
+			shortName: "Mar",
+			value: 3,
+		},
+		"March full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "April"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "April",
+			shortName: "Apr",
+			value: 4,
+		},
+		"April full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "May"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "May",
+			shortName: "May",
+			value: 5,
+		},
+		"May full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "June"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "June",
+			shortName: "Jun",
+			value: 6,
+		},
+		"June full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "July"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "July",
+			shortName: "Jul",
+			value: 7,
+		},
+		"July full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "August"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "August",
+			shortName: "Aug",
+			value: 8,
+		},
+		"August full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "September"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "September",
+			shortName: "Sep",
+			value: 9,
+		},
+		"September full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "October"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "October",
+			shortName: "Oct",
+			value: 10,
+		},
+		"October full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "November"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "November",
+			shortName: "Nov",
+			value: 11,
+		},
+		"November full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "December"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "December",
+			shortName: "Dec",
+			value: 12,
+		},
+		"December full name parsed"
+	);
+});
+
+test("ChronoFieldParser:parse:weekday:en-US", (t) => {
+	const locale = "en-US";
+	const p = new ChronoFieldParser(locale);
+
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Monday"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Monday",
+			shortName: "Mon",
+			value: 1,
+		},
+		"full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "MonDaY"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Monday",
+			shortName: "Mon",
+			value: 1,
+		},
+		"full name parsed in case-insensitive manner"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Mon"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Monday",
+			shortName: "Mon",
+			value: 1,
+		},
+		"short name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "mOn"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Monday",
+			shortName: "Mon",
+			value: 1,
+		},
+		"short name parsed in case insensitive manner"
+	);
+
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Tuesday"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Tuesday",
+			shortName: "Tue",
+			value: 2,
+		},
+		"Tuesday full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Wednesday"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Wednesday",
+			shortName: "Wed",
+			value: 3,
+		},
+		"Wednesday full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Thursday"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Thursday",
+			shortName: "Thu",
+			value: 4,
+		},
+		"Thursday full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Friday"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Friday",
+			shortName: "Fri",
+			value: 5,
+		},
+		"Friday full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Saturday"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Saturday",
+			shortName: "Sat",
+			value: 6,
+		},
+		"Saturday full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.DAY_OF_WEEK, "Sunday"),
+		{
+			field: ChronoField.DAY_OF_WEEK,
+			name: "Sunday",
+			shortName: "Sun",
+			value: 7,
+		},
+		"Sunday full name parsed"
+	);
+});
+
+test("ChronoFieldParser:construct:fr-FR", (t) => {
+	const locale = "fr-FR";
+	const p = new ChronoFieldParser(locale);
+	t.is(p.locale, locale, "locale from constructor arg");
+});
+
+test("ChronoFieldParser:parse:month:fr-FR", (t) => {
+	const locale = "fr-FR";
+	const p = new ChronoFieldParser(locale);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "janvier"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "janvier",
+			shortName: "janv",
+			value: 1,
+		},
+		"full name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "JanVieR"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "janvier",
+			shortName: "janv",
+			value: 1,
+		},
+		"full name parsed in case-insensitive manner"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "janv"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "janvier",
+			shortName: "janv",
+			value: 1,
+		},
+		"short name parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "JanV"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "janvier",
+			shortName: "janv",
+			value: 1,
+		},
+		"short name parsed in case insensitive manner"
+	);
+});
+
+test("ChronoFieldParser:parse:month:fr-FR:accents", (t) => {
+	const locale = "fr-FR";
+	const p = new ChronoFieldParser(locale);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "février"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"full name with accents parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "FÉvRieR"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"full name with accents parsed in case insensitive manner"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "févr"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"short name with accents parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "FÉvR"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"short name with accents parsed in case insensitive manner"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "fevrier"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"full name without accents parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "FevRieR"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"full name without accents parsed in case-insensitive manner"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "fevr"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"short name without accents parsed"
+	);
+	t.like(
+		p.parse(ChronoField.MONTH_OF_YEAR, "FevR"),
+		{
+			field: ChronoField.MONTH_OF_YEAR,
+			name: "février",
+			shortName: "févr",
+			value: 2,
+		},
+		"short name without accents parsed in case-insensitive manner"
+	);
+});

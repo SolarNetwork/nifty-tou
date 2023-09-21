f774ea041b70ebb1c12cb6138b15b79110ca6345
Matt Magoffin <matt@solarnetwork.net>
2023-09-21 15:50:11 +1200

    Add new functions to main entry.

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,10 @@
+export {
+	ChronoField,
+	ChronoFieldValue,
+	ChronoFieldParser,
+} from "./ChronoFieldParser.js";
 export { default as IntRange } from "./IntRange.js";
+export { default as NumberParser } from "./NumberParser.js";
 export { default as TariffRate } from "./TariffRate.js";
 export { default as TemporalRangesTariff } from "./TemporalRangesTariff.js";
 import * as Utils from "./utils.js";

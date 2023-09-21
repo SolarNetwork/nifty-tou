3cd40643d39b2dfb7c9b8bc8aed241e0ebbe0a1c
Matt Magoffin <matt@solarnetwork.net>
2023-09-21 12:12:45 +1200

    Add NumberParser to parse locale number strings.

---
+++ src/main/NumberParser.ts
@@ -0,0 +1,75 @@
+/**
+ * A locale-specific number parser.
+ *
+ * Adapted from Mike Bostock's https://observablehq.com/@mbostock/localized-number-parsing
+ * @public
+ */
+export default class NumberParser {
+	#locale: string;
+	#group: RegExp;
+	#decimal: RegExp;
+	#numeral: RegExp;
+	#index: (d: string) => string;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param locale - the desired locale
+	 */
+	constructor(locale: string) {
+		const parts = new Intl.NumberFormat(locale).formatToParts(12345.6);
+		const numerals = [
+			...new Intl.NumberFormat(locale, { useGrouping: false }).format(
+				9876543210
+			),
+		].reverse();
+		const index = new Map<string, string>(
+			numerals.map((d, i) => [d, i.toString()])
+		);
+		this.#locale = locale;
+		this.#group = new RegExp(
+			`[${parts.find((d) => d.type === "group").value}]`,
+			"g"
+		);
+		this.#decimal = new RegExp(
+			`[${parts.find((d) => d.type === "decimal").value}]`
+		);
+		this.#numeral = new RegExp(`[${numerals.join("")}]`, "g");
+		this.#index = (d) => index.get(d);
+	}
+
+	/** Get the locale. */
+	get locale(): string {
+		return this.#locale;
+	}
+
+	/**
+	 * Normalize a locale-specific number string.
+	 *
+	 * @param s - the number string to parse in this instance's locale
+	 * @returns the number string normalized into a JavaScript number string
+	 */
+	norm(s: string): string {
+		if (s === undefined || s === null) {
+			return undefined;
+		}
+		s = s
+			.trim()
+			.replaceAll("+", "")
+			.replace(this.#group, "")
+			.replace(this.#decimal, ".")
+			.replace(this.#numeral, this.#index);
+		return s || undefined;
+	}
+
+	/**
+	 * Parse a locale-specific number string.
+	 *
+	 * @param s - the number string to parse in this instance's locale
+	 * @returns the parsed number, or `undefined`
+	 */
+	parse(s: string): number {
+		s = this.norm(s);
+		return s ? +s : NaN;
+	}
+}
---
+++ src/test/NumberParserTests.ts
@@ -0,0 +1,129 @@
+import test from "ava";
+import NumberParser from "../main/NumberParser.js";
+
+test("NumberParser:construct", (t) => {
+	const locale = "en-US";
+	const p = new NumberParser(locale);
+	t.is(p.locale, locale, "locale from constructor arg");
+});
+
+test("NumberParser:parse:NaN", (t) => {
+	const locale = "en-US";
+	const p = new NumberParser(locale);
+	t.is(p.parse(undefined), NaN, "undefined returns NaN");
+	t.is(p.parse(null), NaN, "null returns NaN");
+	t.is(p.parse("foo"), NaN, "unparsable returns NaN");
+	t.is(p.parse(""), NaN, "empty returns undefined");
+});
+
+test("NumberParser:parse:en-US:integer", (t) => {
+	const locale = "en-US";
+	const p = new NumberParser(locale);
+	t.is(p.parse("123"), 123, "simple integer parsed");
+	t.is(p.parse("+123"), 123, "signed positive integer parsed");
+	t.is(p.parse("-123"), -123, "signed negative integer parsed");
+});
+
+test("NumberParser:parse:en-US:delimitedInteger", (t) => {
+	const locale = "en-US";
+	const p = new NumberParser(locale);
+	t.is(p.parse("1,234,567"), 1234567, "delimited integer parsed");
+	t.is(
+		p.parse("+1,234,567"),
+		1234567,
+		"signed positive delimited integer parsed"
+	);
+	t.is(
+		p.parse("-1,234,567"),
+		-1234567,
+		"signed negative delimited integer parsed"
+	);
+});
+
+test("NumberParser:parse:en-US:decimal", (t) => {
+	const locale = "en-US";
+	const p = new NumberParser(locale);
+	t.is(p.parse("12.34"), 12.34, "simple decimal parsed");
+	t.is(p.parse("+12.34"), 12.34, "signed decimal integer parsed");
+	t.is(p.parse("-12.34"), -12.34, "signed negative decimal parsed");
+});
+
+test("NumberParser:parse:en-US:delimitedDecimal", (t) => {
+	const locale = "en-US";
+	const p = new NumberParser(locale);
+	t.is(p.parse("1,234.567"), 1234.567, "delimited decimal parsed");
+	t.is(
+		p.parse("+1,234.567"),
+		1234.567,
+		"signed positive delimited decimal parsed"
+	);
+	t.is(
+		p.parse("-1,234.567"),
+		-1234.567,
+		"signed negative delimited decimal parsed"
+	);
+});
+
+test("NumberParser:parse:de:integer", (t) => {
+	const locale = "de";
+	const p = new NumberParser(locale);
+	t.is(p.parse("123"), 123, "simple integer parsed");
+	t.is(p.parse("+123"), 123, "signed positive integer parsed");
+	t.is(p.parse("-123"), -123, "signed negative integer parsed");
+});
+
+test("NumberParser:parse:de:delimitedInteger", (t) => {
+	const locale = "de";
+	const p = new NumberParser(locale);
+	t.is(p.parse("1.234.567"), 1234567, "delimited integer parsed");
+	t.is(
+		p.parse("+1.234.567"),
+		1234567,
+		"signed positive delimited integer parsed"
+	);
+	t.is(
+		p.parse("-1.234.567"),
+		-1234567,
+		"signed negative delimited integer parsed"
+	);
+});
+
+test("NumberParser:parse:de:decimal", (t) => {
+	const locale = "de";
+	const p = new NumberParser(locale);
+	t.is(p.parse("12,34"), 12.34, "simple decimal parsed");
+	t.is(p.parse("+12,34"), 12.34, "signed decimal integer parsed");
+	t.is(p.parse("-12,34"), -12.34, "signed negative decimal parsed");
+});
+
+test("NumberParser:norm:de:delimitedDecimal", (t) => {
+	const locale = "de";
+	const p = new NumberParser(locale);
+	t.is(p.norm("1.234,567"), "1234.567", "delimited decimal normalized");
+	t.is(
+		p.norm("+1.234,567"),
+		"1234.567",
+		"signed positive delimited decimal normalized"
+	);
+	t.is(
+		p.norm("-1.234,567"),
+		"-1234.567",
+		"signed negative delimited decimal normalized"
+	);
+});
+
+test("NumberParser:parse:de:delimitedDecimal", (t) => {
+	const locale = "de";
+	const p = new NumberParser(locale);
+	t.is(p.parse("1.234,567"), 1234.567, "delimited decimal parsed");
+	t.is(
+		p.parse("+1.234,567"),
+		1234.567,
+		"signed positive delimited decimal parsed"
+	);
+	t.is(
+		p.parse("-1.234,567"),
+		-1234.567,
+		"signed negative delimited decimal parsed"
+	);
+});

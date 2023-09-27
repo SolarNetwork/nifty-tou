c67179bad6b1253b034bc8e5dd0200007ba66ea5
Matt Magoffin <matt@solarnetwork.net>
2023-09-27 16:49:47 +1300

    Rebuild.

--- lib/main/YearTemporalRangesTariff.d.ts
+++ lib/main/YearTemporalRangesTariff.d.ts
@@ -1,3 +1,4 @@
+import { ChronoField } from "./ChronoFieldFormatter.js";
 import { default as IntRange } from "./IntRange.js";
 import TariffRate from "./TariffRate.js";
 import { default as TemporalRangesTariff, TemporalRangesTariffFormatOptions } from "./TemporalRangesTariff.js";
@@ -59,6 +60,16 @@ export default class YearTemporalRangesTariff extends TemporalRangesTariff {
      * @override
      */
     compareTo(o: YearTemporalRangesTariff): number;
+    /**
+     * Format a field range into a locale-specific string.
+     *
+     * @param locale - the desired locale
+     * @param field - the field to format
+     * @param options - the formatting options
+     * @returns the formatted field range value
+     * @throws `TypeError` if `field` is not supported
+     */
+    format(locale: string, field: ChronoField, options?: TemporalRangesTariffFormatOptions): string;
     /**
      * Get a string representation of the components of this description.
      * @returns string representation of the components of this tariff
